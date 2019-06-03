17f75c9d63600065dafd7762ddb8d55c212e6391
RW <rinze.watanabe.translation@gmail.com>
2019-06-03 17:05:28 +1000

    Updated ojisantor

--- ojisanator.py
+++ ojisanator.py
@@ -1,5 +1,6 @@
+import random
+
 import MeCab
-import emojis
 import jaconv
 
 chasen = MeCab.Tagger("-Ochasen")
@@ -11,22 +12,55 @@ def parse(sentence):
     return word_list
 
 
-def add_nouns(parsed: list):
-    word_list = []
-    for word in parsed:
+def random_emote():
+    if random.randint(0, 2) == 0:
+        return '😚'
+    else:
+        return '（笑）'
 
-        if '人名' in word[3]:
+
+def add_ojisan(parsed: list):
+    word_list = []
+    for i, word in enumerate(parsed):
+
+        if '人名' and '固有名詞' in word[3]:
             word_list.append(word[0])
             word_list.append('チャン')
 
-        elif '助詞' in word[3]:
+            if random.randint(0, 2) == 0:
+                word_list.append(random_emote())
+
+        elif '係助詞' in word[3]:
             word_list.append(word[0])
+            word_list.append(random_emote())
             word_list.append('、')
 
-        elif '助動詞' in word[3]:
+        elif '助動詞' in word[3] and '特殊' not in word[4]:
+            # catches ない to not kanakanalise it.
             word_list.append(jaconv.hira2kata(word[0]))
+
+        elif '終助詞' in word[3]:
+            word_list.append(jaconv.hira2kata(word[0]))
+            word_list.append(random_emote())
+
+        elif '接尾' and '人名' in word[3]:
+            pass
+
+        elif '接続助詞' in word[3]:
+            word_list.append(word[0])
+            word_list.append(random_emote())
+
+        elif word[0] == ('?' or '？'):
+            word_list.append('❓')
+
+        elif word[0] == ('!' or '！'):
+            word_list.append('❗')
 
         else:
             word_list.append(word[0])
 
     return ''.join(word_list)
+
+
+def ojisanator(sentence):
+    return add_ojisan(parse(sentence))
